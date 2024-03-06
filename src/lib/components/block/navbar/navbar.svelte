f0627199cc727b91a0a611c804feafdc7d9930ac
Aritra Mondal <aritra041999.dev@gmail.com>
2024-03-06 20:33:10 +0100

    Navbar (#2)
    * Add bits-ui support
    
    * Add shadcn button component
    
    * Add shadcn avatar component
    
    * Add shadcn dropdown component
    
    * Update shadcn button component
    
    * Add new auth component
    
    To render logged in session
    
    * Add svg assets for navbar
    
    - add
    - explore
    - home
    - learn
    
    * Add navbar component
    
    * User navbar in root layout

---
+++ src/lib/components/block/navbar/navbar.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import logo from '$lib/images/logo-full.svg';
+	import home from '$lib/images/home.svg';
+	import explore from '$lib/images/explore.svg';
+	import learn from '$lib/images/learn.svg';
+	import add from '$lib/images/add.svg';
+	import { Button } from '$lib/components/ui/button';
+	import Auth from '$lib/components/block/auth/auth.svelte';
+</script>
+
+<div class="fixed h-16 w-screen">
+	<div
+		class="container flex h-full items-center justify-between border-b border-dotted border-zinc-300"
+	>
+		<div>
+			<img src={logo} alt="duggup" class="h-6" />
+		</div>
+		<div class="flex items-center space-x-8">
+			<div class="flex space-x-8">
+				<a href="/home" class="flex items-center text-sm text-zinc-500">
+					<img src={home} alt="home" class="mr-2 h-4" />
+					Home
+				</a>
+				<a href="/explore" class="flex items-center text-sm text-zinc-500">
+					<img src={explore} alt="explore" class="mr-2 h-4" />
+					Explore
+				</a>
+				<a href="/learn" class="flex items-center text-sm text-zinc-500">
+					<img src={learn} alt="learn" class="mr-2 h-4" />
+					Learn
+				</a>
+			</div>
+			<div>
+				<Button variant="outline">
+					<img src={add} alt="learn" class="mr-2 h-4" />
+					Post
+				</Button>
+			</div>
+			<div>
+				<Auth />
+			</div>
+		</div>
+	</div>
+</div>
