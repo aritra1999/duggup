2001bdaca031d48f9e25508e2bf47f1c03a945d6
Aritra <aritra041999.dev@gmail.com>
2024-03-07 23:44:29 +0100

    Add activity component and activity section in profile page

---
+++ src/lib/components/block/profile/activity.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { BlogActivity, ExperienceActivity } from '$lib/components/block/profile/utils';
+
+	export let activity: (BlogActivity | ExperienceActivity)[] = [];
+</script>
+
+{#if activity}
+	<div class="flex flex-col space-y-4">
+		{#each activity as item}
+			{item}
+		{/each}
+	</div>
+{:else}
+	<div class="text-center">No activity</div>
+{/if}
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,7 +1,9 @@
 <script class="ts">
+	import Activity from '$lib/components/block/profile/activity.svelte';
 	import ProfileHead from '$lib/components/block/profile/head.svelte';
 
 	export let data;
 </script>
 
 <ProfileHead profile={data.profile} />
+<Activity activity={data.activity} />
