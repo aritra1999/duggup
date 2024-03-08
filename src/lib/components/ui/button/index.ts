e3c935b1096e3c35e4b7571a801086a5f0541cda
Aritra <aritra041999.dev@gmail.com>
2024-03-08 17:05:44 +0100

    Update shadcn outline button variant

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -9,7 +9,7 @@ const buttonVariants = tv({
 			default: 'bg-primary text-primary-foreground hover:bg-primary/90',
 			destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 			outline:
-				'border border-zinc-500 border-b-4 bg-background hover:bg-accent hover:text-accent-foreground',
+				'border border-zinc-500 border-b-[5px] bg-background hover:bg-blue-600 hover:border-blue-800 hover:text-white',
 			secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
 			ghost: 'hover:bg-accent hover:text-accent-foreground',
 			link: 'text-primary underline-offset-4 hover:underline'
