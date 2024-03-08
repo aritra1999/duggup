6f8a15fe70f92656534fefffefb077ee5e8f80a4
Aritra <aritra041999.dev@gmail.com>
2024-03-08 17:08:05 +0100

    Update navbar

--- src/lib/components/block/navbar/navbar.svelte
+++ src/lib/components/block/navbar/navbar.svelte
@@ -8,7 +8,7 @@
 	import Auth from '$lib/components/block/auth/auth.svelte';
 </script>
 
-<div class="fixed h-16 w-screen">
+<div class="fixed z-50 h-16 w-screen bg-background">
 	<div
 		class="container flex h-full items-center justify-between border-b border-dotted border-zinc-300"
 	>
@@ -17,15 +17,15 @@
 		</div>
 		<div class="flex items-center space-x-8">
 			<div class="flex space-x-8">
-				<a href="/home" class="flex items-center text-sm text-zinc-500">
+				<a href="/home" class="flex items-center text-xs text-zinc-500">
 					<img src={home} alt="home" class="mr-2 h-4" />
 					Home
 				</a>
-				<a href="/explore" class="flex items-center text-sm text-zinc-500">
+				<a href="/explore" class="flex items-center text-xs text-zinc-500">
 					<img src={explore} alt="explore" class="mr-2 h-4" />
 					Explore
 				</a>
-				<a href="/learn" class="flex items-center text-sm text-zinc-500">
+				<a href="/learn" class="flex items-center text-xs text-zinc-500">
 					<img src={learn} alt="learn" class="mr-2 h-4" />
 					Learn
 				</a>
