c61e66c3ce744a292bdcbae8919031e9f940ac96
Aritra <aritra041999.dev@gmail.com>
2024-03-09 09:56:17 +0100

    UI Cleanup: Move section wrapper to parent component
    For
    - Profile head
    - Profile activities

--- src/lib/components/block/profile/activities.svelte
+++ src/lib/components/block/profile/activities.svelte
@@ -5,14 +5,12 @@
 	export let activityMap: Record<string, (BlogActivity | ExperienceActivity)[]> = {};
 </script>
 
-<section class="w-10/12">
-	{#if activityMap}
-		<div class="flex flex-col">
-			{#each Object.entries(activityMap) as [timeStamp, activities]}
-				<Activity {activities} {timeStamp} />
-			{/each}
-		</div>
-	{:else}
-		<div class="text-center">No activity</div>
-	{/if}
-</section>
+{#if activityMap}
+	<div class="flex flex-col">
+		{#each Object.entries(activityMap) as [timeStamp, activities]}
+			<Activity {activities} {timeStamp} />
+		{/each}
+	</div>
+{:else}
+	<div class="text-center">No activity</div>
+{/if}
