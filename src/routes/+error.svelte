2ce13cd6eebba5aa308cf6e47125c3329367f9fd
Aritra <aritra041999.dev@gmail.com>
2024-03-09 10:06:08 +0100

    Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex h-screen w-full items-center justify-center overflow-hidden">
+	<div class="fixed text-[40vw] font-black text-slate-200 dark:text-slate-800">{$page.status}</div>
+	<div class="fixed">{$page.error?.message}</div>
+</div>
