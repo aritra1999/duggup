e56307c0192ebfa3e7cd4f8bfded4120b450a19d
Aritra <aritra041999.dev@gmail.com>
2024-03-09 09:57:08 +0100

    Unified common styles
    to reduce repetition

--- src/lib/components/block/profile/blogs.svelte
+++ src/lib/components/block/profile/blogs.svelte
@@ -2,6 +2,7 @@
 	import c1 from '$lib/images/c1.svg';
 	import type { BlogActivity } from '$lib/components/block/profile/utils';
 	import * as Card from '$lib/components/ui/card';
+	import '$lib/components/block/profile/profile.pcss';
 
 	export let activities: BlogActivity[] = [];
 	export let timeStamp: string;
@@ -9,11 +10,13 @@
 
 {#if activities}
 	<div class="flex w-full items-center">
-		<div class="w-[190px] pr-2 text-end text-xs text-muted-foreground">{timeStamp}</div>
-		<div class="flex h-full w-[50px] justify-center">
+		<div class="left-container text-xs">
+			{timeStamp}
+		</div>
+		<div class="middle-container">
 			<img src={c1} alt="" class="z-40 h-3 w-3" />
 		</div>
-		<div class="-ml-[22px] w-full border-l border-dotted border-zinc-300 pb-3 pl-[30px] pt-6">
+		<div class="right-container pb-3 pt-6">
 			<div class="columns-3xs gap-9">
 				{#each activities as activity}
 					<Card.Root class="mb-9 last:mb-0">
--- src/lib/components/block/profile/experiences.svelte
+++ src/lib/components/block/profile/experiences.svelte
@@ -3,6 +3,7 @@
 	import type { ExperienceActivity } from '$lib/components/block/profile/utils';
 	import * as Card from '$lib/components/ui/card';
 	import { ChevronUp } from 'lucide-svelte';
+	import '$lib/components/block/profile/profile.pcss';
 
 	export let activities: ExperienceActivity[] = [];
 	export let timeStamp: string;
@@ -11,7 +12,7 @@
 {#if activities}
 	{#each activities as activity}
 		<div class="flex items-center">
-			<div class="w-[190px] pr-2 text-end font-normal text-muted-foreground">
+			<div class="left-container">
 				<div class="flex flex-col items-end">
 					<img
 						src={activity.organization.logo}
@@ -24,13 +25,11 @@
 				</div>
 				<div class="text-xs">{timeStamp}</div>
 			</div>
-			<div class="relative flex w-[50px] justify-center">
-				<ChevronUp class="absolute -top-16 left-[10px] h-4 w-4 text-muted-foreground" />
-				<img src={c2} alt="" class="z-40 h-5 w-5" />
+			<div class="middle-container relative">
+				<ChevronUp class="absolute -top-16 left-[13px] h-4 w-4 text-muted-foreground" />
+				<img src={c2} alt="" class="z-40 h-5 w-5 text-muted-foreground" />
 			</div>
-			<div
-				class="-ml-[22px] flex h-40 w-full items-center border-l border-dotted border-zinc-300 pl-[30px]"
-			>
+			<div class="right-container flex h-40 items-center">
 				<Card.Root class="border-0">
 					<Card.Content>
 						<h5>{activity.organization.role}</h5>
